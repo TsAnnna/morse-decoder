9fa6370a932e3a541efb99d5b0416426b49079cb
Anna <green.abrikos.73@gmail.com>
2022-08-12 22:25:51 +0300

    add task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,19 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result = '';
+    let arr = [];
+
+    for(let i = 0; i < Math.ceil(expr.length / 10); i++) {
+        arr[i] = parseInt(expr.slice(i * 10, (i * 10) + 10)).toString();
+    }
+
+    let arr1 =  arr.map(element => element.replace(/11/g, "-"));
+    arr1 =  arr1.map(element => element.replace(/10/g, "."));
+    arr1.forEach(element => result += MORSE_TABLE[element]);
+    result = result.replace(/undefined/g, ' ');
+    return result;
+
 }
 
 module.exports = {
